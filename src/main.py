b9c6b8876351394176879725406d587bfe585363
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-12-22 09:58:12 +0100

    functionality: clone all owned repos

--- src/main.py
+++ src/main.py
@@ -1,1 +1,30 @@
-print("main")
+import os
+import subprocess
+from datetime import datetime
+from dotenv import load_dotenv
+from github import Github, Auth
+
+load_dotenv()
+
+token = os.getenv("GITHUB_TOKEN")
+github = Github(auth=Auth.Token(token))
+folder = "github-" + datetime.now().strftime("%Y-%m-%d")
+
+print("create folder: " + folder)
+os.chdir(os.path.expanduser("~/Downloads/"))
+os.mkdir(folder)
+
+for repo in github.get_user().get_repos(affiliation="owner"):
+	print("git clone: " + repo.name)
+	subprocess.run(
+		[
+			"git",
+			"clone",
+			f"git@github.com:{repo.full_name}.git",
+			f"{folder}/{repo.name}",
+		],
+		capture_output=True,
+	)
+
+print("done")
+github.close()
